c244088031fec7bb45da03bf79b6e82c6ddefb14
EnzoWolf <allen@enzowolf.com>
2021-11-16 15:20:00 -0500

    Completed Iteration Statements & Loops Exercise1

---
+++ IterationStatementsExercise1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IterationStatementsExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write this for 15 times (muscle memory)
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
